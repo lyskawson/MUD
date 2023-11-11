1bb912e9ac43daa16a10713cd8e9c0ee375911c7
Aleksander Łyskawa <alek.lyskawa@gmail.com>
2023-11-11 01:43:07 +0100

    Change parameters

--- LAB5/lyskawaLab5.m
+++ LAB5/lyskawaLab5.m
@@ -14,20 +14,31 @@ du = 1;
 xDot0 = 0;
 x0 = 0;
 
-%%1) parametry z przygotowania
+
+%%1) wartosci parametrow z przygotowania
 ksi_1 = [3, -13/5, 0, 1/5, -1/4];
 titles_1 = ["ksi = 3", "ksi = -13/5", "ksi = 0", "ksi = 1/5", "ksi = -1/4"];
-colors=['r','b','m','g','c'];
+colors=['r','b','g','m','c'];
 w=7;
 
 for i=1:length(ksi_1)
    ksi=ksi_1(i);
    lambda1=w*(-ksi + sqrt(ksi * ksi - 1));
    lambda2=w*(-ksi - sqrt(ksi * ksi -1));
+   if ksi == -13/5 || ksi == -1/4
+       startTime = 6;
+       stopTime = 16;
+   else 
+       startTime = 0;
+       stopTime = 10;
+
+   end 
+
    figure;
    subplot(211);
    sim(model);
-   plot(ans.tout,ans.x, colors(i), LineWidth=2);grid on;
+   plot(ans.tout,ans.x, colors(i), LineWidth=2);
+   grid on;
    title('Rozwiazanie i bieguny dla ',titles_1(i));
    xlabel('t');
    ylabel('x');
@@ -44,14 +55,18 @@ for i=1:length(ksi_1)
    ylabel('Im');
 end
 
-%%2) Dobrane wartosci parametrow
+
+%%2) Dobrane wartosci parametrow do drugiej czesci zadania
 ksi_2=[0.3,0.4,0.8,0.9];
-w_2=[2,3.5,4,5.5];
+w_2=[2,5,8,10];
+startTime = 0;
+stopTime = 7;
+
 
 %%takie samo ksi
 figure;
 for i=1:length(ksi_2)
-ksi=ksi_2(3);
+ksi=ksi_2(2);
 w=w_2(i);
 lambda1=w*(-ksi + sqrt(ksi * ksi - 1));
 lambda2=w*(-ksi - sqrt(ksi * ksi -1));
@@ -67,7 +82,6 @@ ylabel('x');
 subplot(212);
 title('Bieguny');
 hold on;
-ax.YAxisLocation = 0;
 plot(real(lambda1),imag(lambda1),strcat('x',colors(i)), LineWidth=5);
 plot(real(lambda2),imag(lambda2),strcat('x',colors(i)), LineWidth=5);
 line([0,0], ylim, 'Color', 'k', LineWidth=2);
@@ -106,7 +120,9 @@ title('Bieguny');
 end
 
 %%takie samo re
-% dla re(lambda = -1.5 )
+% dla re(lambda) = -1.5 
+% ze wzoru -ksi*w=-1.5
+
 w_3= 1.5./(ksi_2);
 figure;
 for i=1:length(ksi_2)
@@ -115,38 +131,7 @@ w=w_3(i);
 lambda1=w*(-ksi + sqrt(ksi * ksi - 1));
 lambda2=w*(-ksi - sqrt(ksi * ksi -1));
 subplot(211);
-title('Taka sama część rzeczywista (odległość od osi).')
-hold on;
-sim(model);
-plot(ans.tout,ans.x,colors(i), LineWidth=2);
-grid on;
-xlabel('t');
-ylabel('x');
-
-subplot(212);
-hold on;
-plot(real(lambda1),imag(lambda1),strcat(colors(i),'x'),'LineWidth',5);
-plot(real(lambda2),imag(lambda2),strcat(colors(i),'x'),'LineWidth',5);
-line([0,0], ylim, 'Color', 'k', LineWidth=2);
-line(xlim, [0,0], 'Color', 'k', LineWidth=2); 
-grid on;
-xlabel('Re');
-ylabel('Im');
-title('Bieguny');
-end
-
-%%przyklad ze skryptu 
-ksi_s=[0.9,0.9, 0.4,0.4];
-w_s=[5,3,3,11.25];
-
-figure;
-for i=1:length(ksi_s)
-ksi=ksi_s(i);
-w=w_s(i);
-lambda1=w*(-ksi + sqrt(ksi * ksi - 1));
-lambda2=w*(-ksi - sqrt(ksi * ksi -1));
-subplot(211);
-title('Wykres ze skryptu')
+title('Taka sama część rzeczywista (odległość od osi OX).')
 hold on;
 sim(model);
 plot(ans.tout,ans.x,colors(i), LineWidth=2);
